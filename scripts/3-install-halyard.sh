f718439ed1ad08af52f99c8acade937ea0d91c81
root <root@DEN34758L.corp.gds.jeppesen.com>
2018-08-24 14:48:32 -0600

    scripts for spinnaker demo

---
+++ scripts/3-install-halyard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
+sudo bash InstallHalyard.sh --user ubuntu
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker ubuntu
+
+hal config storage edit --type s3
+
